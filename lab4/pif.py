d1fd309ea8e83edc4f43f752c30711180bb746f8
Harangus-Dan-Ioan <33331174+danharangus@users.noreply.github.com>
2023-11-03 19:02:30 +0200

    Delete lab4 directory

--- lab4/pif.py
+++
@@ -1,9 +0,0 @@
-class Pif:
-    def __init__(self):
-        self.pif = []
-    
-    def add(self, token, pos):
-        self.pif.append((token, pos))
-
-    def __str__(self):
-        return "\n".join(f"Token: {token}, Position: {pos}" for token, pos in self.pif)
