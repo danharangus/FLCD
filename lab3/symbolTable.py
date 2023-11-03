fbbe1841d78dea6e1d733bb8015a01034dfc7804
Harangus-Dan-Ioan <33331174+danharangus@users.noreply.github.com>
2023-11-03 18:57:36 +0200

    Add files via upload

---
+++ lab3/symbolTable.py
@@ -0,0 +1,22 @@
+from hashTable import HashTable
+
+class SymbolTable:
+    def __init__(self, size):
+        self.table = HashTable(size)
+        self.crt = 1
+
+    def insert(self, name):
+        pos = self.get(name)
+        if pos is not None:
+            return pos
+        self.table.put(name, self.crt)
+        self.crt += 1
+        return self.crt - 1
+
+    def get(self, name):
+        return self.table.get(name)
+
+    def display(self):
+        return self.table.__str__()
+    
+
